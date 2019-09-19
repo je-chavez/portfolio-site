e31cdd662768755e7d86f0f4dbd7c00c5d88ce1a
Juan Chavez <juanchavez017@gmail.com>
2019-09-19 11:22:55 -0700

    Added a footer with logos

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import customTheme from '../theme/theme';
+import { Typography } from '@material-ui/core';
 
 const styles = makeStyles({
-    mainDivFooter: {
-        height: '150px',
-        width: '100%',
-        backgroundColor: customTheme.colors.background
-    }
+  mainDivFooter: {
+    height: '200px',
+    width: '100%',
+    backgroundColor: customTheme.colors.background,
+    textAlign: 'center',
+    paddingTop: '50px'
+  },
+  logo: {
+    color: customTheme.colors.font_primary,
+    '&:hover': {
+      color: customTheme.colors.primary
+    },
+    padding: '20px'
+  },
+  name: {
+    color: customTheme.colors.font_primary,
+    fontFamily: customTheme.fontStyle,
+    fontSize: '14px',
+    paddingTop: '20px'
+  }
 });
 
 export default function Footer(props) {
@@ -15,6 +31,40 @@ export default function Footer(props) {
 
   return (
     <div className={classes.mainDivFooter}>
+      <a className={classes.logo} href='https://github.com/je-chavez' target='_blank'>
+        <ion-icon
+          name='logo-github'
+          size='large'
+          style={{
+            padding: '8px',
+            border: 'solid 1px',
+            borderRadius: '50%'
+          }}></ion-icon>
+      </a>
+      <a className={classes.logo} href='https://linkedin.com/in/juanenriquechavez' target='_blank'>
+        <ion-icon
+          name='logo-linkedIn'
+          size='large'
+          style={{
+            padding: '8px',
+            border: 'solid 1px',
+            borderRadius: '50%'
+          }}></ion-icon>
+      </a>
+      <a className={classes.logo} href='https://instagram.com/juandollah' target='_blank'>
+        <ion-icon
+          name='logo-instagram'
+          size='large'
+          style={{
+            padding: '8px',
+            border: 'solid 1px',
+            borderRadius: '50%'
+          }}></ion-icon>
+      </a>
+      <Typography className={classes.name} variant='body1'>
+        JUAN CHAVEZ •{' '}
+        <span style={{ color: customTheme.colors.primary }}>2019</span>
+      </Typography>
     </div>
   );
 }
